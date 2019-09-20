a3ac233daf9d924854cb6e1004ee49c7eeafa5dd
hgmanchego <46489545+hgmanchego@users.noreply.github.com>
2019-09-20 13:16:21 -0500

    Add files via upload

---
+++ Binario a decimal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int len(int x){
+	int y=0;
+	while(x!=0){
+		x/=10;
+		y++;
+	}
+	return y;
+}
+int power(int x,int y){
+	if(y==0)
+		return 1;
+	else			
+		return x*power(x,y-1);
+}     
+int main(int argc, char *argv[]){
+	int x,y,z=0,size,sum=0;
+	cout<<"Ingrese numero binario: ";cin>>x;
+	size=len(x);
+	for(int i=0;i<size;i++){
+		y=x%10;
+		sum=sum+(y*power(2,z));
+		x/=10;
+		z++;
+	}
+	cout<<"En decimal seria: "<<sum;
+	return 0;
+}
+
---
+++ Decimal a binario.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int len(int a){
+	int b=0;
+	while(a!=0){
+		a/=2;
+		b++;
+	}
+	return b;
+}
+int power(int x,int y){
+	if(y==0)
+		return 1;
+	else
+		return x*power(x,y-1);
+}    
+int main(int argc, char *argv[]) {
+	int x,a,b=0,size,size2;
+	cout<<"Ingrese numero: ";cin>>x;
+	size=len(x);
+	size2=size;
+	for(int i=0;i<size2;i++){
+		a=x%2;
+		b=b+a*power(10,i);
+		x/=2;
+	}
+	cout<<b;
+	return 0;
+}
+
